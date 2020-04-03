813137caf44c0fd373c447be1965e5069e23f0fa
Sammantha Sadler <sammantha.sadler@gmail.com>
2020-04-02 22:35:11 -0600

    add: graph with conversion data

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Graph from '../Graph/Graph';
 import Numbers from '../Numbers/Numbers';
 
-const data = require('../logs.json');
+const logs = require('../logs.json');
 
 const Data = props => {
 
-	const totals = employeeTotals(props.id);
+	const data = employeeData(props.id);
+	const dataConversions = data.filter(point => point.type === 'conversion');
+	const totals = employeeTotals(data);
 
 	return (
 		<div>
-			<Graph />
+			<Graph data={dataConversions}/>
 			<Numbers impressions = {totals.impressions} conversions={totals.conversions} revenue={totals.revenue} />
 		</div>
 	);
@@ -18,13 +20,12 @@ const Data = props => {
 
 //Iterate through the JSON data and keep only the data that belongs to this employee
 const employeeData = id => {
-	const dataArray = [...data];
+	const dataArray = [...logs];
 	return dataArray.filter(point => point.user_id === id);
 }
 
 //Get the totals from the employee-specific data
-const employeeTotals = id => {
-	const filtered = employeeData(id);
+const employeeTotals = filtered => {
 	let impressions = 0;
 	let conversions = 0;
 	let revenue = 0;
--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { LineChart, Line, Tooltip } from 'recharts';
 
-const Graph = () => {
+const Graph = props => {
+
+	const allData = props.data.sort(function(x, y){
+    	return x.time - y.time;
+	});
+
+	const data = allData.slice(allData.length - 15, allData.length - 1);
+
+	const divStyle = {
+		margin: 'auto auto'
+	};
+
 	return (
-		<div className="graph"><p>This is a graph</p></div>
+		<div className="graph">
+			<LineChart
+		        width={300}
+		        height={130}
+		        data={data}
+		        style={divStyle}
+		    	>
+		        <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+	     	</LineChart>
+		</div>
 	);
 }
 
